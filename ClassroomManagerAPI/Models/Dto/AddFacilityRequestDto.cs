e44a3d164c446c7de304160e37cdcc3c00bf0ba0
PluckySquirrel <hoanganhluong2@gmail.com>
2024-03-25 08:17:58 +0700

    added serilog and crud endpoints

---
+++ ClassroomManagerAPI/Models/Dto/AddFacilityRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClassroomManagerAPI.Models.Dto
+{
+	public class AddFacilityRequestDto
+	{
+		[Required]
+        public string Name { get; set; }
+		[Required]
+		public int Count { get; set; }
+		public string Status { get; set; }
+		public string Version { get; set; }
+		public string Details { get; set; }
+	}
+}
--- ClassroomManagerAPI/Program.cs
+++ ClassroomManagerAPI/Program.cs
@@ -2,10 +2,19 @@ using AutoMapper;
 using ClassroomManagerAPI;
 using ClassroomManagerAPI.Data;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var logger = new LoggerConfiguration()
+	.WriteTo.Console()
+	.MinimumLevel.Information()
+	.CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
+
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
