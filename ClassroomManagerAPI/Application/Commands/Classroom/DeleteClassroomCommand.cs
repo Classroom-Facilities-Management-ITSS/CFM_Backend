0c234d8b2eb24381a730cca900cf73508f0893ca
PluckySquirrel <hoanganhluong2@gmail.com>
2024-06-03 15:31:05 +0700

    updated storageclassroom in deleteclassroomcommand

--- ClassroomManagerAPI/Application/Commands/Classroom/DeleteClassroomCommand.cs
+++ ClassroomManagerAPI/Application/Commands/Classroom/DeleteClassroomCommand.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using ClassroomManagerAPI.Application.Commands.Facility;
 using ClassroomManagerAPI.Common;
 using ClassroomManagerAPI.Enums.ErrorCodes;
 using ClassroomManagerAPI.Repositories.IRepositories;
@@ -15,23 +16,59 @@ namespace ClassroomManagerAPI.Application.Commands.Classroom
 	public class DeleteClassroomCommandHandler : IRequestHandler<DeleteClassroomCommand, ResponseMethod<string>>
 	{
 		private readonly IClassroomRepository _classroomRepository;
+		private readonly IFacilityRepository _facilityRepository;
+		private readonly Guid _storageClassroomId;
+		private readonly IMediator _mediator;
 
-		public DeleteClassroomCommandHandler(IClassroomRepository classroomRepository)
+		public DeleteClassroomCommandHandler(IClassroomRepository classroomRepository, IMediator mediator, IFacilityRepository facilityRepository)
         {
 			_classroomRepository = classroomRepository;
+			_storageClassroomId = Guid.Parse("0540dec7-c15c-4d3d-9b24-a1cfca346209");
+			_mediator = mediator;
+			_facilityRepository = facilityRepository;
 		}
 
 		public async Task<ResponseMethod<string>> Handle(DeleteClassroomCommand request, CancellationToken cancellationToken)
 		{
 			ArgumentNullException.ThrowIfNull(request);
 			ResponseMethod<string> result = new ResponseMethod<string>();
-			var classroom = _classroomRepository.Queryable().Include(x => x.Schedules).Any(x => (!x.IsDeleted) && (x.FacilityAmount != 0 || x.Schedules.Any(f => f.EndTime <= DateTime.Now.AddDays(7))));
-			if (classroom)
+			var classroom = await _classroomRepository.Queryable()
+				.Include(x => x.Facilities)
+				.Include(x => x.Schedules)
+				.FirstOrDefaultAsync(x => x.Id == request.Id && !x.IsDeleted);
+			if (classroom == null)
+			{
+				result.AddBadRequest(nameof(ErrorSystemEnum.DataNotExist));
+				result.StatusCode = (int)HttpStatusCode.NotFound;
+				return result;
+			}
+			var hasConflicts = classroom.FacilityAmount != 0 || classroom.Schedules.Any(f => f.EndTime <= DateTime.Now.AddDays(7));
+			//var classroom = _classroomRepository.Queryable().Include(x => x.Schedules).Any(x => (!x.IsDeleted) && (x.FacilityAmount != 0 || x.Schedules.Any(f => f.EndTime <= DateTime.Now.AddDays(7))));
+			if (hasConflicts)
 			{
 				result.AddBadRequest(nameof(ErrorClassEnum.ConflictSchedule));
 				result.StatusCode = (int)HttpStatusCode.Conflict;
 				return result;
 			}
+			var storageClassroom = await _classroomRepository.GetByIDAsync(_storageClassroomId);
+			if (storageClassroom == null)
+			{
+				result.AddBadRequest(nameof(ErrorSystemEnum.DataNotExist));
+                result.StatusCode = (int)HttpStatusCode.NotFound;
+                return result;
+			}
+			foreach (var facility in classroom.Facilities)
+			{
+				facility.ClassroomId = _storageClassroomId;
+				//await _facilityRepository.UpdateAsync(facility).ConfigureAwait(false);
+				await _mediator.Send(new UpdateFacilityCommand { ClassroomId = facility.ClassroomId }).ConfigureAwait(false);
+			}
+			storageClassroom.FacilityAmount += classroom.FacilityAmount;
+			classroom.FacilityAmount = 0;
+			//await _classroomRepository.UpdateAsync(storageClassroom).ConfigureAwait(false);
+			await _mediator.Send(new UpdateClassroomCommand { Address = storageClassroom.Address }).ConfigureAwait(false);
+			// await _classroomRepository.UpdateAsync(classroom).ConfigureAwait(false);
+			await _mediator.Send(new UpdateClassroomCommand { Address = classroom.Address }).ConfigureAwait(false);
 			var deletedClassrooom = await _classroomRepository.DeleteAsync(request.Id).ConfigureAwait(false);
 			if (!deletedClassrooom)
 			{
--- ClassroomManagerAPI/Application/Commands/Facility/DeleteFacilityCommand.cs
+++ ClassroomManagerAPI/Application/Commands/Facility/DeleteFacilityCommand.cs
@@ -1,4 +1,5 @@
-﻿using ClassroomManagerAPI.Common;
+﻿using ClassroomManagerAPI.Application.Commands.Classroom;
+using ClassroomManagerAPI.Common;
 using ClassroomManagerAPI.Enums.ErrorCodes;
 using ClassroomManagerAPI.Repositories.IRepositories;
 using MediatR;
@@ -15,13 +16,15 @@ namespace ClassroomManagerAPI.Application.Commands.Facility
     {
         private readonly IFacilityRepository _facilityRepository;
         private readonly IClassroomRepository _classroomRepository;
-        private readonly Guid _storageClassroomId;
+		private readonly IMediator _mediator;
+		private readonly Guid _storageClassroomId;
 
-        public DeleteFacilityCommandHandler(IFacilityRepository facilityRepository, IClassroomRepository classroomRepository)
+        public DeleteFacilityCommandHandler(IFacilityRepository facilityRepository, IClassroomRepository classroomRepository, IMediator mediator)
         {
             _facilityRepository = facilityRepository;
             _classroomRepository = classroomRepository;
-            _storageClassroomId = Guid.Parse("0540dec7-c15c-4d3d-9b24-a1cfca346209");
+			_mediator = mediator;
+			_storageClassroomId = Guid.Parse("0540dec7-c15c-4d3d-9b24-a1cfca346209");
         }
         public async Task<ResponseMethod<string>> Handle(DeleteFacilityCommand request, CancellationToken cancellationToken)
         {
@@ -36,7 +39,8 @@ namespace ClassroomManagerAPI.Application.Commands.Facility
             }
             var currentClassroom = await _classroomRepository.GetByIDAsync(existingFacility.ClassroomId).ConfigureAwait(false);
 			currentClassroom.FacilityAmount -= existingFacility.Count;
-            await _classroomRepository.UpdateAsync(currentClassroom).ConfigureAwait(false);
+            //await _classroomRepository.UpdateAsync(currentClassroom).ConfigureAwait(false);
+            await _mediator.Send(new UpdateClassroomCommand { Address = currentClassroom.Address }).ConfigureAwait(false);
             var storageClassroom = await _classroomRepository.GetByIDAsync(_storageClassroomId).ConfigureAwait(false);
             if(storageClassroom == null)
             {
@@ -45,9 +49,11 @@ namespace ClassroomManagerAPI.Application.Commands.Facility
 				return result;
 			}
             existingFacility.ClassroomId = _storageClassroomId;
-            await _facilityRepository.UpdateAsync(existingFacility).ConfigureAwait(false);
+            // await _facilityRepository.UpdateAsync(existingFacility).ConfigureAwait(false);
+            await _mediator.Send(new UpdateFacilityCommand { ClassroomId = existingFacility.ClassroomId }).ConfigureAwait(false);
             storageClassroom.FacilityAmount += existingFacility.Count;
-            await _classroomRepository.UpdateAsync(storageClassroom).ConfigureAwait(false);
+            // await _classroomRepository.UpdateAsync(storageClassroom).ConfigureAwait(false);
+            await _mediator.Send(new UpdateClassroomCommand { Address = storageClassroom.Address }).ConfigureAwait(false);
 
             await _facilityRepository.DeleteAsync(request.Id).ConfigureAwait(false);
             result.StatusCode = (int) HttpStatusCode.OK;
